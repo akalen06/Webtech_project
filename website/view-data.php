b235a44b08f81bc3e12f2dea6c296e94b29bad7f
akalen06 <lennert.engelen@gmail.com>
2025-06-18 17:43:38 +0200

    Added basic CSS styling and linked it to index.html and view-data.php

--- website/view-data.php
+++ website/view-data.php
@@ -2,21 +2,7 @@
 <html>
 <head>
     <title>Temperatuur Data Overzicht</title>
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
+    <link rel="stylesheet" href="style.css">
 <body>
     <h1>Temperatuur Data Overzicht</h1>
 
